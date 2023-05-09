f7daac3ffe6001b1856a827328cfceab81ed41fe
ZachyDev <mosetizachary001@gmail.com>
2023-05-09 15:57:55 +0300

    Function Basic

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('testing');
+let greet;
+greet = () => {
+    console.log('Hello user');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 25);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,20 @@
-console.log('test')
+let greet: Function;
+
+greet = () => {
+    console.log('Hello user');
+}
+
+
+const add = (a:number,b:number,c:number|string = 10) => {
+    console.log(a+b)
+    console.log(c)
+}
+
+add(5,10,25);
+
+let minus: Function;
+
+minus = (b:number,c:number): number => {
+    return b + c;
+}
+let result = minus(20,9);
